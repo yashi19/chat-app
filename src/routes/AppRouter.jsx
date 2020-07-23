65512d39a3843d6b0f223419e56aa3e4d05bc3df
Yashika Tanwar <yashikatanwar@Yashikas-MacBook-Pro.local>
2020-07-23 21:17:54 +0530

    Add history and navigation method

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Login from "../components/Login/Login";
 import App from "../components/App/App";
+import {history} from "./history";
 
 const AppRouter = () => {
     return (
-        <Router>
+        <Router history={history}>
             <Switch>
                 <Route path={"/"} exact={true}>
                     <Login />
---
+++ src/routes/navigation.js
@@ -0,0 +1,5 @@
+import {history} from "./history";
+
+export const goToRoute = (route) => {
+    history.push(route);
+}
